a22ed340449d7289ee6dfdb88e5447083a5cc95e
양찬희 <chjh0929@gmail.com>
2018-05-26 23:33:34 +0900

    퍼블리싱 세팅 수정, 퍼블리싱 키스토어 생성 (PW : WestPig).

--- New Unity Project/Assets/GooglePlayGames/GameInfo.cs
+++ New Unity Project/Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "957264039090"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "957264039090-9c2u1m09p13ipt4qb8ehnj74aglmhvjd.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
